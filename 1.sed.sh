e8c1e365cee06025ae732c69f9d9d7f285fe72ef
Manish Mani <manishpamu@hotmail.com>
2025-09-24 12:16:44 +0530

    Update 1.sed.sh

--- 1.sed.sh
+++ 1.sed.sh
@@ -1,1 +1,3 @@
-
+sed -n 'p' file.txt         # Print every line (useless alone, but useful with conditions)
+sed -n '3p' file.txt        # Print only line 3
+sed -n '1, 3p' file.txt     # print 1 - 3 lines
